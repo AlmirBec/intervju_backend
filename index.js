0123f6afbfaad3b0b5ccae181d3bc69f585f2fe8
Almir Becirovic <almircodeb@gmail.com>
2023-08-23 15:35:30 +0200

    fixes

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ app.use(express.json())
 const port = 3001;
 
 app.post("/sort-numbers", (req, res) => {
-
-    let arr = req.body.numbers;
+    let objekt = req.body;
+    let arr = []
+    for(const key in objekt){
+        arr.push(objekt[key]);
+    }
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = parseInt(arr[i]);
+    }
     let sortedArr = funkcije.sortArray(arr);
     res.json({sortedArr})
-    
+
 });
 
 
